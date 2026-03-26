e0718c3ba8b6dd8fd43f808fe8412403831d9c79
Juan Sebastian Valencia <juan.quinchua@correounivalle.edu.co>
2026-03-26 18:50:23 -0500

    Se modifico el module-info.java para que puede acceder a los controladores del programa

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,7 @@ module com.example.sol_eclipsado {
 
 
     opens com.example.sol_eclipsado to javafx.fxml;
+    opens com.example.sol_eclipsado.controller to javafx.fxml;
+
     exports com.example.sol_eclipsado;
 }
